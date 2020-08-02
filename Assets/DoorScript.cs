646dc9c4a30b7792f36924a9063e04404780323d
hohi1114 <31657166+hohi1114@users.noreply.github.com>
2020-08-02 14:39:12 +0900

    init

---
+++ Assets/DoorScript.cs
@@ -0,0 +1,49 @@
+﻿
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorScript : MonoBehaviour
+{
+    private Animator anim;
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim=GetComponent<Animator>();
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        anim.SetTrigger("OpenDoor");
+    }
+
+    // void OnTriggerExit(Collider other)
+    // {
+    //     if (other.tag == "Player")
+    //     {
+    //         anim.SetBool("open", false);
+    //         OpenPanel.SetActive(false);
+    //     }
+    // }
+
+    // private bool IsOpenPanelActive
+    // {
+    //     get
+    //     {
+    //         return OpenPanel.activeInHierarchy;
+    //     }
+    // }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // if (IsOpenPanelActive)
+        // {
+        //     if (Input.GetKeyDown(KeyCode.E))
+        //     {
+        //         OpenPanel.SetActive(false);
+        //         anim.SetBool("open", true);
+        //     }
+        // }
+    }
+}

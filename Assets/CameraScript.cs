7e498e61474641a1be2fddacd8b0a3981aec9218
IamHyemin <heyhyemin@kaist.ac.kr>
2020-08-03 17:05:48 +0900

    꾸미기

---
+++ Assets/CameraScript.cs
@@ -0,0 +1,40 @@
+﻿
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraScript : MonoBehaviour
+{
+    private Animator anim;
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim=GetComponent<Animator>();
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        anim.SetTrigger("CameraMove");
+    }
+
+    void OnTriggerExit(Collider other)
+     {
+       
+        anim.enabled=true;
+ }
+
+    void pauseAnimationEvent(){
+        anim.enabled=false;
+    }
+    void Update()
+    {
+        // if (IsOpenPanelActive)
+        // {
+        //     if (Input.GetKeyDown(KeyCode.E))
+        //     {
+        //         OpenPanel.SetActive(false);
+        //         anim.SetBool("open", true);
+        //     }
+        // }
+    }
+}

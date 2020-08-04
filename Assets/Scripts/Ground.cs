fb04325f8e099e37e43957880cb56e7d02f269ee
hohi1114 <31657166+hohi1114@users.noreply.github.com>
2020-08-04 17:27:21 +0900

    dd

---
+++ Assets/Scripts/Ground.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ground : MonoBehaviour
+{
+
+    public GameManager manager;
+     void OnTriggerStay(Collider _col)  //STAY
+    {
+
+        Debug.Log("Collide");
+    
+        if (Input.GetKeyDown(KeyCode.G) && manager.items["shovel"] ){
+           gameObject.SetActive(false);
+        }
+        if( Input.GetKeyDown(KeyCode.G) && !(manager.items["shovel"]) ){
+            Debug.Log("You do not have a shovel");
+        }
+    }
+
+}

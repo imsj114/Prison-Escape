2ccea0bfd7ae1bca9bd940ab1a5b05b37aba008a
Seongjae Im <seongjae114@gmail.com>
2020-08-04 22:09:52 +0900

    changing menu

--- Assets/GameManager.cs
+++ Assets/GameManager.cs
@@ -3,10 +3,9 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
-using UnityEngine.SceneManagement;
+using UnityEngine.SceneManagement;    
 
-public class GameManager : MonoBehaviour
-{
+public class GameManager : MonoBehaviour {
     public static GameManager instance {
         get {
             if(m_instance == null){
@@ -17,31 +16,76 @@ public class GameManager : MonoBehaviour
     }
     
     private static GameManager m_instance;
-    
-    public bool isGameover;
-    
-
-public class GameManager : MonoBehaviour {
     private GameObject player;
+    private bool isGameOver = true;
 
     public Dictionary<string, bool> items= new Dictionary<string, bool>();
+    
+
+    string[] itemStrings =
+        {
+            "cellKey",
+            "doorKey",
+            "outKey",
+            "shovel",
+            "hammer",
+            "cardKey",
+            "bread",
+            "meat",
+            "cheese"
+        };
 
     void Awake() {
         if(instance != this) {
             Destroy(gameObject);
         }
-        else {
-            items.Add("cellKey",false);
-            items.Add("doorKey",false);
-            items.Add("outKey",false);
-            items.Add("shovel",false);
-            items.Add("hammer",false);
-            items.Add("cardKey",false);
-            items.Add("bread",false);
-            items.Add("meat",false);
-            items.Add("cheese",false);
+        else
+        {
+            foreach(string itemName in itemStrings){
+            items.Add(itemName, false);
+            }
+            // TODO: Go to menu scene
+        }
+        
+    }
 
-            isGameover = false;
+    void Update() {
+        if(Input.GetButtonDown("Cancel"))
+            paused = togglePause();
+    }
+
+    public void StartGame()
+    {
+        isGameOver = false;
+        foreach(string itemName in itemStrings){
+            items[itemName] = false;
+        }
+        // TODO: Go to start scene
+    }
+
+    public void GameOver(bool escaped = false)
+    {
+        isGameOver = true;
+        if(escaped) {
+            // TODO: Player wins!
+        } else {
+            // TODO: Player loses
         }
     }
+
+    private bool paused = false;
+    private bool togglePause(){
+        if(Time.timeScale == 0f) {
+            Time.timeScale = 1f;
+            // TODO: Popup pause menu
+            return false;
+        }
+        else
+        {
+            Time.timeScale = 0f;
+            // TODO: Remove pause menu
+            return true;
+        }
+    }
+
 }

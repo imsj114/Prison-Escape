3088356878fd4b8b37617df0483c31510275fd69
Seongjae Im <seongjae114@gmail.com>
2020-08-04 17:31:45 +0900

    added preload scene

--- Assets/GameManager.cs
+++ Assets/GameManager.cs
@@ -1,42 +1,42 @@
-﻿/*
+﻿
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
-​
-public class GameManager : MonoBehaviour
-{
-    public static GameManager instance {
-        get {
-            if(m_instance == null){
-                m_instance = FindObjectOfType<GameManager>();
-            }
-            return m_instance;
-        }
+
+// Reference: https://stackoverflow.com/questions/35890932/unity-game-manager-script-works-only-one-time
+// Use _preload scene as the starting (not destroyed throughout the scene).
+
+public class GameManager : MonoBehaviour {
+    private GameObject player;
+
+    public enum Item
+    {
+        cellKey, doorKey, outKey, shovel, cardKey
     }
-    
-    public bool isGameover { get; private set; }
-    private static GameManager m_instance;
-    
 
-    private Player player1;
-    public boolean digged=false;
-
-
-    public Item cellKey, doorKey, outKey, shovel,cardKey;
-
-    public Movable patrol;
+    public bool[] itemPicked = new bool[Enum.GetNames(typeof(Item)).Length];
     
     void Awake() {
-        if(instance != this) {
-            Destroy(gameObject);
-        }else {
-            player1 = GameObject.FindGameObjectsWithTag("Player")[0].GetComponent<Player>();
-            isGameover = false;
-        }
+        // player = GameObject.FindGameObjectsWithTag("Player")[0];
+        // On awake, go to menu scene.
     }
 
+    bool isGameOver = true;
+    public void StartGame()
+    {
+        // Game started. Initialize all variables
+        isGameOver = false;
+        
+        // Load the first scene in the game.
+    }
+
+    public void GameOver()
+    {
+        isGameOver = true;
+        // Game is over!
+    }
 
 }
-*/

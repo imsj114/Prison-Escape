27f8afd9af169cb0fe24556e313c925ef21910d5
IamHyemin <heyhyemin@kaist.ac.kr>
2020-08-04 17:31:10 +0900

    다이알로그

---
+++ Assets/Dialogue/DialogueManager.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DialogueManager : MonoBehaviour
+{
+    private Queue<string> sentences;
+    public Text nameText;
+    public Text dialogueText;
+    public Animator animator;
+
+
+    void Start()
+    {
+        sentences = new Queue<string>();
+    }
+
+    public void StartDialogue(Dialogue dialogue)
+    {
+        animator.SetBool("IsOpen", true);
+        sentences.Clear();
+
+        nameText.text = dialogue.name;
+        foreach(string sentence in dialogue.sentences)
+        {
+            sentences.Enqueue(sentence);
+        }
+        while (sentences.Count != 0){
+            DisplayNextSentence();
+        }
+    }
+
+    public void DisplayNextSentence ()
+    {
+        if (sentences.Count == 0)
+        {
+            EndDialogue();
+            return;
+        }
+        string sentence = sentences.Dequeue();
+        dialogueText.text = sentence;
+    }
+
+
+    void EndDialogue()
+    {
+        animator.SetBool("IsOpen", false);
+    }
+
+
+}
